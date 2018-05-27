ad16a78ec76d2437b5855864cef9e561f355f3c8
ykshinde <shinde.yogesh@gmail.com>
2018-05-27 16:55:08 +0530

    gy
    khkjhkhk

---
+++ app/src/main/java/com/example/ykshinde/sitemanagement/LabourActivity.java
@@ -0,0 +1,15 @@
+package com.example.ykshinde.sitemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class LabourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labour);
+
+    }
+}
---
+++ app/src/main/java/com/example/ykshinde/sitemanagement/MaterialActivity.java
@@ -0,0 +1,24 @@
+package com.example.ykshinde.sitemanagement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+
+    }
+
+
+}
